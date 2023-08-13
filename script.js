a62e5c9b3116d009dc7525929709b9b33abdee7b
yungshun317 <seraph317@gmail.com>
2023-08-13 12:15:46 +0800

    [FEAT] Generate random dice roll and display it

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 'use strict';
 
 // Select elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 let scores, currentScore, activePlayer, playing;
 
@@ -21,6 +26,25 @@ const init = function() {
     score1El.textContent = 0;
     current0El.textContent = 0;
     current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
 }
+init();
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function() {
+   if (playing) {
+       // [1] Generate a random dice roll
+       const dice = Math.trunc(Math.random() * 6) + 1;
+
+       // [2] Display dice
+       diceEl.classList.remove('hidden');
+       diceEl.src = `dice-${dice}.png`;
+
+       // [3] Check for rolled 1
+   }
+});
 
 btnNew.addEventListener('click', init);
