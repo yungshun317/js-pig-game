d04867602a878d3e57b0a487e3bed705d81736c7
yungshun317 <seraph317@gmail.com>
2023-08-13 12:03:55 +0800

    [FEAT] Set the initial state

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// Select elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const btnNew = document.querySelector('.btn--new');
+
+let scores, currentScore, activePlayer, playing;
+
+// Starting conditions
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+}
+
+btnNew.addEventListener('click', init);
